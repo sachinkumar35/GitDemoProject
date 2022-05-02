6fdfcec1f7a5f64692144fa5792413182881bf2e
sachin <sachin@digital.com>
2022-05-02 14:24:46 +0530

    First Commit....

---
+++ src/gitDemo/DemoProject.java
@@ -0,0 +1,15 @@
+package gitDemo;
+
+public class DemoProject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String mongo = "mongos";
+		if(mongo.equals("mongos")) {
+			System.out.println(true);
+		}
+		else System.out.println(false);
+	}
+
+}
